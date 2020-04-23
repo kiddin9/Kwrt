34251f7edf4c141772a8d6952f4ae9236bdba0e7
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-04-23 11:08:46 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -17,6 +17,7 @@ svn co https://github.com/jsda/packages2/trunk/ntlf9t/luci-app-advancedsetting
 git clone https://github.com/lisaac/luci-app-dockerman
 svn co https://github.com/coolsnowwolf/packages/trunk/sound/forked-daapd
 svn co https://github.com/openwrt/luci/trunk/applications/luci-app-sqm
+git clone https://github.com/garypang13/r8125
 
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall
 svn co https://github.com/Lienol/openwrt-package/trunk/package/tcping
