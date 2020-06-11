32e14929fb5fcdaa21a3f711ba60e72063d57c19
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-06-11 23:51:35 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -42,7 +42,7 @@ svn co https://github.com/coolsnowwolf/packages/trunk/net/strongswan
 cd -
 
 mv -f feeds/luci/applications/luci-app-aria2 feeds/custom/luci/luci-app-aria2
-mv -f feeds/packages/net/aria2 feeds/packages/net/aria2
+mv -f feeds/packages/net/aria2 feeds/custom/luci/aria2
 git clone https://github.com/librespeed/speedtest files/www/speedtest
 mv files/www/speedtest/example-singleServer-full.html files/www/speedtest/index.html
 
