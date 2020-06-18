c6bf8aa0c39ef3425fadc9c49a47c53e5185bc52
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-06-18 14:50:20 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -52,6 +52,7 @@ sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' package/feeds/custo
 
 rm -Rf package/*/*/autocore
 rm -Rf package/*/*/qBittorrent/patches
+rm -Rf package/*/*/luci-app-zerotier/root/etc/init.d/zerotier
 rm -Rf files/usr/share/amule/webserver/AmuleWebUI-Reloaded && git clone https://github.com/MatteoRagni/AmuleWebUI-Reloaded files/usr/share/amule/webserver/AmuleWebUI-Reloaded
 rm -Rf files/usr/share/aria2 && git clone https://github.com/P3TERX/aria2.conf files/usr/share/aria2
 rm -Rf package/*/*/antileech/src/* && git clone https://github.com/persmule/amule-dlp.antiLeech package/feeds/custom/antileech/src
