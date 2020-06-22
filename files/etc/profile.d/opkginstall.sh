1c97f35a5c78f548e2fcec1d72a7a47e506f573b
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-06-22 23:02:52 +0800

    Update opkginstall.sh

--- files/etc/profile.d/opkginstall.sh
+++ files/etc/profile.d/opkginstall.sh
@@ -5,13 +5,12 @@ rm -Rf /lib/upgrade/keep.d/php7*
 sed -i 's/service_start $PROG -y/service_start $PROG -R -y/g' /etc/init.d/php7-fpm
 sed -i "s/user =.*/user = root/g" /etc/php7-fpm.d/www.conf
 /etc/init.d/php7-fpm restart
-sed -i 's/extra_setting\"/extra_settings\"/g' /usr/lib/lua/luci/model/cbi/aria2/config.lua
-chmod +x /usr/share/aria2/*.sh
 ln -sf /usr/bin/python3 /usr/bin/python
-ln -sf /usr/lib/netdata/conf.d /etc/netdata/conf.d
+sed -i 's/extra_setting\"/extra_settings\"/g' /usr/lib/lua/luci/model/cbi/aria2/config.lua
 sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/aria2.lua
 sed -i 's/entry({"admin", "nas", "aria2"}/entry({"admin", "nas"}, firstchild(), _("NAS"), 45).dependent = false\nentry({"admin", "nas", "aria2"}/g' /usr/lib/lua/luci/controller/aria2.lua
 sed -i 's/services/nas/g' /usr/lib/lua/luci/view/aria2/*.htm
+sed -i 's/ariang/ariang-nginx/g' /usr/lib/lua/luci/view/aria2/settings_header.htm
 sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/hd_idle.lua
 sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/samba4.lua
 sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/ksmbd.lua
