2aa6b1fa8fc6c136dbde0f5585a0d544d0b78097
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-06-22 21:09:32 +0800

    Update opkginstall.sh

--- files/etc/profile.d/opkginstall.sh
+++ files/etc/profile.d/opkginstall.sh
@@ -9,10 +9,6 @@ sed -i 's/extra_setting\"/extra_settings\"/g' /usr/lib/lua/luci/model/cbi/aria2/
 chmod +x /usr/share/aria2/*.sh
 ln -sf /usr/bin/python3 /usr/bin/python
 ln -sf /usr/lib/netdata/conf.d /etc/netdata/conf.d
-ln /usr/lib/netdata/conf.d/charts.d.conf /etc/netdata/charts.d.conf
-ln /usr/lib/netdata/conf.d/python.d.conf /etc/netdata/python.d.conf
-ln -f /etc/netdata/charts.d.conf /usr/lib/netdata/conf.d/charts.d.conf
-ln -f /etc/netdata/python.d.conf /usr/lib/netdata/conf.d/python.d.conf
 sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/aria2.lua
 sed -i 's/entry({"admin", "nas", "aria2"}/entry({"admin", "nas"}, firstchild(), _("NAS"), 45).dependent = false\nentry({"admin", "nas", "aria2"}/g' /usr/lib/lua/luci/controller/aria2.lua
 sed -i 's/services/nas/g' /usr/lib/lua/luci/view/aria2/*.htm
