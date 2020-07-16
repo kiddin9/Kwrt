d72ece2f1d9ea8e94f2dda7138d8fccb3d1737f3
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-07-16 09:28:23 +0800

    Update diy.sh

--- common/diy.sh
+++ common/diy.sh
@@ -112,10 +112,6 @@ find package/*/custom/*/ -maxdepth 2 ! -path "*shadowsocksr-libev*" -name "Makef
 find package/*/custom/*/ -maxdepth 2 -name "Makefile" | xargs -i sed -i "s/SUBDIRS=/M=/g" {}
 sed -i 's/$(VERSION) &&/$(VERSION) ;/g' include/download.mk
 sed -i '/PKG_BUILD_DIR.*(PKG_NAME)/d' feeds/luci/luci.mk
-sed -i "/foreach pkg/d" feeds/luci/luci.mk;
-sed -i 's/$(INCLUDE_DIR)\/package.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' package/*/*/luci-app-*/Makefile
-sed -i "/foreach pkg/d" package/*/*/luci-*/Makefile
-find package/*/*/luci-*/ package/*/*/luci/ -maxdepth 1 -name Makefile | xargs -i sed -i '$a $(foreach pkg,$(LUCI_BUILD_PACKAGES),$(eval $(call BuildPackage,$(pkg))))' {}
 find package/*/custom/*/ -maxdepth 1 -d -name "i18n" | xargs -i rename -v 's/i18n/po/' {}
 find package/*/custom/*/ -maxdepth 2 -d -name "zh-cn" | xargs -i rename -v 's/zh-cn/zh_Hans/' {}
 sed -i "/po2lmo /d" package/*/custom/*/Makefile
