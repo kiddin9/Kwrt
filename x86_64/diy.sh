0d30f4dcbad35dd60c0da992eccbb8ed4622f08a
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-07-19 02:31:28 +0800

    Update diy.sh

--- x86_64/diy.sh
+++ x86_64/diy.sh
@@ -3,4 +3,3 @@
 sed -i "/bin\/upx/d" package/*/*/*/Makefile
 sed -i "s/linux\//linux64\//g" package/*/*/coremark/Makefile
 rm -Rf feeds/custom/luci/mt
-rm -Rf files/usr/share/amule/webserver/AmuleWebUI-Reloaded && git clone https://github.com/MatteoRagni/AmuleWebUI-Reloaded files/usr/share/amule/webserver/AmuleWebUI-Reloaded
