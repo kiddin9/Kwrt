31716c972c30338a5d8ebb27d75e660aaa262d1d
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-07-26 10:08:32 +0800

    Update diy.sh

--- k2p/diy.sh
+++ k2p/diy.sh
@@ -1,3 +1,4 @@
 rm -Rf files/etc/profile.d/sysinfo.sh
 find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
 svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
+wget -P https://github.com/privacy-protection-tools/anti-AD/raw/master/anti-ad-smartdns.conf files/etc/smartdns/anti-ad-smartdns.conf
