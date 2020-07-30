ee7f0a06d210dc5fd9d51505109dfdf36a622c26
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-07-30 16:06:46 +0800

    Update diy.sh

--- common/diy.sh
+++ common/diy.sh
@@ -90,7 +90,6 @@ sed -i 's/ @!BUSYBOX_DEFAULT_IP:/ +/g' package/*/*/wrtbwmon/Makefile
 sed -i 's/root\/Download/data\/download\/aria2/g' files/usr/share/aria2/*
 sed -i '/resolvfile=/d' package/*/*/luci-app-adguardhome/root/etc/init.d/AdGuardHome
 sed -i 's/DEPENDS:=/DEPENDS:=+adguardhome /g' package/*/*/luci-app-adguardhome/Makefile
-sed -i 's/ +nginx-mod-luci-ssl//g' package/*/*/luci-ssl-nginx/Makefile
 sed -i '/EXTRA_DEPENDS:=nginx-util/d' package/*/*/nginx-util/Makefile
 sed -i '/_redirect2ssl/d' package/*/*/nginx/Makefile
 sed -i '/init_lan/d' package/*/*/nginx/files/nginx.init
