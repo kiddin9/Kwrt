7ab5c5f50aedd21f9695ed5e64b49bdb69cce21e
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-09-13 09:14:29 +0800

    Update diy.sh

--- common/diy.sh
+++ common/diy.sh
@@ -7,7 +7,6 @@ rm -Rf feeds/packages/net/miniupnpd
 mv -f feeds/packages/libs/libx264 package/feeds/custom/libx264
 mv -f feeds/packages/net/aria2 package/feeds/custom/aria2
 mv -f feeds/packages/admin/netdata package/feeds/custom/netdata
-mv -f feeds/packages/net/smartdns package/feeds/custom/smartdns
 
 rm -Rf tools/upx && svn co https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
 rm -Rf tools/ucl && svn co https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
