6cc04d409c9a1fcd56960a0eca9f6ba0a943732d
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-09-25 01:43:06 +0800

    Update diy.sh

--- redmi_ac2100/diy.sh
+++ redmi_ac2100/diy.sh
@@ -1,3 +1,2 @@
-rm -Rf files/etc/profile.d/sysinfo.sh
 find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
