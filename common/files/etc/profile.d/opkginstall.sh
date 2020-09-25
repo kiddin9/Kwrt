3a77c737b4578545bb332d5348f424015824fb46
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-09-26 01:15:36 +0800

    Update opkginstall.sh

--- common/files/etc/profile.d/opkginstall.sh
+++ common/files/etc/profile.d/opkginstall.sh
@@ -18,6 +18,7 @@ sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/minidlna.lua
 sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/transmission.lua
 sed -i 's/entry({"admin", "nas", "aria2"}/entry({"admin", "nas"}, firstchild(), _("NAS"), 45).dependent = false\nentry({"admin", "nas", "aria2"}/g' /usr/lib/lua/luci/controller/aria2.lua
 sed -i 's/local page = entry/entry({"admin", "nas"}, firstchild(), _("NAS"), 45).dependent = false\nlocal page = entry/g' /usr/lib/lua/luci/controller/transmission.lua
+sed -i 's/page = entry/entry({"admin", "nas"}, firstchild(), _("NAS"), 45).dependent = false\npage = entry/g' /usr/lib/lua/luci/controller/hd_idle.lua
 sed -i 's/entry/entry({"admin", "nas"}, firstchild(), _("NAS"), 45).dependent = false\nentry/g' /usr/lib/lua/luci/controller/ksmbd.lua
 sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/mjpg-streamer.lua
 sed -i 's/services/nas/g'  /usr/lib/lua/luci/view/minidlna_status.htm
