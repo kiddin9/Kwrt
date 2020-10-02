54968216aa5617d9d272f5c9a1c7ded7e2bd3079
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-10-02 16:16:46 +0800

    update

--- r2s/diy.sh
+++ r2s/diy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sed -i 's/Os/O2/g' include/target.mk
-wget -O target/linux/rockchip/patches-5.4/998-rockchip-enable-i2c0-on-NanoPi-R2S.patch https://raw.githubusercontent.com/QiuSimons/R2S-OpenWrt/master/PATCH/new/main/998-rockchip-enable-i2c0-on-NanoPi-R2S.patch
-wget -O /target/linux/rockchip/patches-5.4/999-unlock-1608mhz-rk3328.patch https://github.com/QiuSimons/R2S-OpenWrt/blob/master/PATCH/new/main/999-unlock-1608mhz-rk3328.patch
-wget -O target/linux/rockchip/patches-5.4/998-rockchip-enable-i2c0-on-NanoPi-R2S.patch https://raw.githubusercontent.com/QiuSimons/R2S-OpenWrt/master/PATCH/new/main/998-rockchip-enable-i2c0-on-NanoPi-R2S.patch
+
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 sed -i '/;;/i\set_interface_core 8 "ff160000" "ff160000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
 sed -i '/;;/i\set_interface_core 1 "ff150000" "ff150000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
