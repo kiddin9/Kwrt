695b8ad5b71e67b5d4a68aa787fcf2643a5c8f43
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-10-12 22:31:37 +0800

    update

--- phicomm-k2p/diy.sh
+++ phicomm-k2p/diy.sh
@@ -1,4 +1,5 @@
 rm -Rf files/etc/profile.d/sysinfo.sh
 find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
+rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
 sed -i 's/O2/Os/g' include/target.mk
