3e0113a325a7b89c04efcb0db30f0fb0be41ddf3
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-10-20 01:57:19 +0800

    update

--- devices/hiwifi-hc5962/diy.sh
+++ devices/hiwifi-hc5962/diy.sh
@@ -1,5 +1,4 @@
-rm -Rf target/linux/ramips
-rm -Rf target/linux/ramips/.svn target/linux/ramips/files/drivers/net/ethernet/ralink
-echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
-svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/files/drivers/net/ethernet/ralink target/linux/ramips/files/drivers/net/ethernet/ralink
+rm -Rf target/linux/generic target/linux/ramips target/linux/generic/.svn target/linux/ramips/.svn
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic target/linux/generic
 
--- devices/phicomm-k2p/diy.sh
+++ devices/phicomm-k2p/diy.sh
@@ -1,6 +1,5 @@
 rm -Rf files/etc/profile.d/sysinfo.sh
-rm -Rf target/linux/ramips
-rm -Rf target/linux/ramips/.svn target/linux/ramips/files/drivers/net/ethernet/ralink
-echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
-svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/files/drivers/net/ethernet/ralink target/linux/ramips/files/drivers/net/ethernet/ralink
+rm -Rf target/linux/generic target/linux/ramips target/linux/generic/.svn target/linux/ramips/.svn
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic target/linux/generic
 sed -i 's/O2/Os/g' include/target.mk
