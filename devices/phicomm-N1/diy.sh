21042c0947d9db7513c7db67a80ecf8b4228b194
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-10-20 02:42:48 +0800

    update

--- devices/phicomm-N1/diy.sh
+++ devices/phicomm-N1/diy.sh
@@ -1,2 +1,2 @@
 sed -i "s/+luci\( \|$\)//g"  package/*/*/*/Makefile
-rm -Rf devices/common/patches/syncdial.patch devices/common/patches/disable_flock.patch devices/common/diy/package/network/services
+
