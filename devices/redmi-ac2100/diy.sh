35601dcd16a67f1196b56011ca01f5507f044143
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-10-25 03:17:14 +0800

    update

--- devices/redmi-ac2100/diy.sh
+++ devices/redmi-ac2100/diy.sh
@@ -1,3 +1,3 @@
-rm -Rf target/linux/generic target/linux/ramips
-svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
-svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic target/linux/generic
+find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
+rm -Rf target/linux/ramips/.svn
+echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
