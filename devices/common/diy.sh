848768fcdefc551d56ee185da8447bde0f8f22f9
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-10-26 23:32:42 +0800

    update

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -86,3 +86,4 @@ sed -i "/mediaurlbase/d" package/*/*/luci-theme*/root/etc/uci-defaults/*
 date=`date +%m.%d.%Y`
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by GaryPang'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
+sed -i '$a cgi-timeout = 300' package/feeds/packages/uwsgi/files-luci-support/luci-webui.ini
