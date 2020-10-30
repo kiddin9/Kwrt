6dbed05780f7c609e234b04cd898d1b5216bd9b3
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-10-30 12:57:34 +0800

    Update opkginstall.sh

--- devices/common/files/etc/profile.d/opkginstall.sh
+++ devices/common/files/etc/profile.d/opkginstall.sh
@@ -2,7 +2,7 @@
 
 opkg() {
     if [[ `echo $@ | grep -o -E '^install'` ]]; then
-	command opkg $@
+	command opkg --force-checksum --force-overwrite $@
 #rm -Rf /lib/upgrade/keep.d/php7*
 #sed -i 's/service_start $PROG -y/service_start $PROG -R -y/g' /etc/init.d/php7-fpm
 #sed -i "s/user =.*/user = root/g" /etc/php7-fpm.d/www.conf
@@ -11,9 +11,9 @@ opkg() {
 	[[ ! "`pgrep UnblockNeteaseMusic`" && "`uci get unblockmusic.@unblockmusic[0].enabled`" == 1 ]] && {
 	/etc/init.d/unblockmusic restart
 	}
-	rm -Rf /tmp/luci-modulecache /tmp/luci-indexcache*
+	rm -Rf /tmp/luci-*
     else
-        command opkg $@
+        command opkg --force-checksum --force-overwrite $@
     fi
     rm -f /var/lock/opkg.lock
 }
