af6eadccb33a761835e96018668152708296e48e
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-11-15 10:10:48 +0800

    Update diy.sh

--- devices/nanopi-r2s/diy.sh
+++ devices/nanopi-r2s/diy.sh
@@ -8,6 +8,9 @@ wget -O target/linux/rockchip/patches-5.4/104-rockchip-rk3328-add-i2c0-controlle
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 sed -i '/;;/i\set_interface_core 8 "ff160000" "ff160000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
 sed -i '/;;/i\set_interface_core 1 "ff150000" "ff150000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
+sed -i 's/ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305/ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256/' package/feeds/custom/*/*/*/*/*/*.lua
+sed -i 's/TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256/TLS_CHACHA20_POLY1305_SHA256:TLS_AES_128_GCM_SHA256/' package/feeds/custom/*/*/*/*/*/*.lua
+
 echo '
 CONFIG_ARM64_CRYPTO=y
 CONFIG_CRYPTO_AES_ARM64=y
