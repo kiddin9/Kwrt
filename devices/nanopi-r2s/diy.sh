c0a3b3972c57330075e991b6a925f49a04f577c3
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-12-12 21:37:44 +0800

    update

--- devices/nanopi-r2s/diy.sh
+++ devices/nanopi-r2s/diy.sh
@@ -2,10 +2,12 @@
 
 find target/linux/rockchip/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
 rm -Rf target/linux/rockchip/.svn
-echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/rockchip target/linux/rockchip
+echo -e "\q" | svn co https://github.com/project-openwrt/openwrt/trunk/target/linux/rockchip target/linux/rockchip
 
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
+sed -i '/;;/i\set_interface_core 8 "ff160000" "ff160000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
+sed -i '/;;/i\set_interface_core 1 "ff150000" "ff150000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
 sed -i '/;;/i\ethtool -K eth0 rx off tx off && logger -t disable-offloading "disabed rk3328 ethernet tcp/udp offloading tx/rx"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
 
 sed -i 's/ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305/ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256/' package/feeds/custom/luci-app-ssr-plus/root/usr/share/ssrplus/gentrojanconfig
