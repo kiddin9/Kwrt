f7e3f5ee44feaeca64349e11c5a06b912f777344
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-12-20 20:52:18 +0800

    update

--- devices/phicomm-k2p/diy.sh
+++ devices/phicomm-k2p/diy.sh
@@ -4,7 +4,7 @@ find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}'
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
 rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
-rm -Rf target/linux/ramips/patches-5.4/006-rockchip-rk3328-add-compatible-to-NanoPi-R2S-etherne.patch
+
 sed -i 's/O2/Os/g' include/target.mk
 
 sed -i 's/PKG_VERSION:=1/PKG_VERSION:=2/' package/feeds/custom/luci-app-ssr-plus/Makefile
