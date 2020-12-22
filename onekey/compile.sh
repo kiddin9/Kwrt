c7698fc26d9282b906dcd81a3b8e0993c6cdf3d8
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-12-22 14:26:31 +0800

    update

--- onekey/compile.sh
+++ onekey/compile.sh
@@ -115,13 +115,20 @@ read -p "请输入后台地址 [回车默认10.0.0.1]: " ip
 ip=${ip:-"10.0.0.1"}
 echo "您的后台地址为: $ip"
 
-./scripts/feeds update -a
-
 if [ -f "devices/common/feeds.conf" ]; then
           mv devices/common/feeds.conf ./
 fi       
 if [ -f "devices/$firmware/feeds.conf" ]; then
           mv devices/$firmware/feeds.conf ./
+fi
+./scripts/feeds update -a
+if [ -f "devices/common/diy.sh" ]; then
+		chmod +x devices/common/diy.sh
+		/bin/bash "devices/common/diy.sh"
+fi
+if [ -f "devices/$firmware/diy.sh" ]; then
+		chmod +x devices/$firmware/diy.sh
+		/bin/bash "devices/$firmware/diy.sh"
 fi
 if [ -n "$(ls -A "devices/common/files" 2>/dev/null)" ]; then
 	cp -rf devices/common/files files
@@ -134,14 +141,6 @@ if [ -n "$(ls -A "devices/common/diy" 2>/dev/null)" ]; then
 fi
 if [ -n "$(ls -A "devices/$firmware/diy" 2>/dev/null)" ]; then
 	cp -Rf devices/$firmware/diy/* ./
-fi
-if [ -f "devices/common/diy.sh" ]; then
-		chmod +x devices/common/diy.sh
-		/bin/bash "devices/common/diy.sh"
-fi
-if [ -f "devices/$firmware/diy.sh" ]; then
-		chmod +x devices/$firmware/diy.sh
-		/bin/bash "devices/$firmware/diy.sh"
 fi
 if [ -f "devices/common/default-settings" ]; then
 	sed -i 's/10.0.0.1/$ip/' devices/common/default-settings
