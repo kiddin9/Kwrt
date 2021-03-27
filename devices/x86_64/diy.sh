99e5dc54f2342d0102bebad9df6ccc62ee63b7a2
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-03-27 22:48:09 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -4,3 +4,5 @@ echo '
 CONFIG_CRYPTO_CHACHA20_X86_64=y
 CONFIG_CRYPTO_POLY1305_X86_64=y
 ' >> ./target/linux/x86/64/config-5.4
+
+sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
