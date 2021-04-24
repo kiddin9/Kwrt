14ad60b99f763a7cc743462c88ce3670012d0137
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-04-24 17:08:00 +0800

    Update compile.sh

--- onekey/compile.sh
+++ onekey/compile.sh
@@ -185,7 +185,8 @@ if [ -f sdk1.tar.xz ]; then
 	sed -i 's,$(STAGING_DIR_HOST)/bin/upx,upx,' package/feeds/custom/*/Makefile
 fi
 
-make -j$(($(nproc)+1)) download v=s ; make -j$(($(nproc)+1)) || make -j1 V=s
+make -j$(($(nproc)+1)) download -j$(($(nproc)+1)) &
+make -j$(($(nproc)+1)) || make -j1 V=s
 
 if [ "$?" == "0" ]; then
 echo "
