ac23092951c120595628d7394c3f46a817d35ecc
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-05-08 20:00:13 +0800

    update

--- devices/phicomm-k2p/diy.sh
+++ devices/phicomm-k2p/diy.sh
@@ -1,4 +1,16 @@
 
+find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
+echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
+rm -Rf target/linux/ramips/.svn
+echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
+
+rm -rf package/feeds/custom/mt-drivers
+svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/lean/mt-drivers package/feeds/custom/mt-drivers
+
+sed -i 's/kmod-mt7615d_dbdc/kmod-mt7615d luci-app-mtwifi/g' target/linux/ramips/image/mt7621.mk b/target/linux/ramips/image/mt7621.mk
+
+sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
+
 rm -Rf files/etc/profile.d/sysinfo.sh
 
 sed -i 's/O2/Os/g' include/target.mk
--- devices/redmi-ac2100/diy.sh
+++ devices/redmi-ac2100/diy.sh
@@ -3,9 +3,4 @@ echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ra
 rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
 
-rm -rf target/linux/ramips/files/drivers/net/ethernet/ralink
-svn co https://github.com/x-wrt/x-wrt/trunk/target/linux/ramips/files/drivers/net/ethernet/ralink target/linux/ramips/files/drivers/net/ethernet/ralink
-
-wget -O target/linux/ramips/patches-5.4/990-mtk-driver-hwnat-compat-with-natflow.patch https://raw.githubusercontent.com/x-wrt/x-wrt/master/target/linux/ramips/patches-5.4/990-mtk-driver-hwnat-compat-with-natflow.patch
-
 sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
