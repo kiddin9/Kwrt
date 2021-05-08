0e974e5caada69138a65b05a269da5de956c1283
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-05-08 20:02:45 +0800

    update

--- devices/k2p-32m-usb/diy.sh
+++ devices/k2p-32m-usb/diy.sh
@@ -6,7 +6,7 @@ echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ra
 rm -rf package/feeds/custom/mt-drivers
 svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/lean/mt-drivers package/feeds/custom/mt-drivers
 
-sed -i 's/kmod-mt7615d_dbdc/kmod-mt7615d luci-app-mtwifi/g' target/linux/ramips/image/mt7621.mk b/target/linux/ramips/image/mt7621.mk
+sed -i 's/kmod-mt7615d_dbdc/kmod-mt7615d luci-app-mtwifi/g' target/linux/ramips/image/mt7621.mk
 
 sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
 
