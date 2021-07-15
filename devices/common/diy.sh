6d1b4d42b281d436422a34ea3e718f753ffd750c
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-07-15 11:00:01 +0800

    update

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -7,9 +7,9 @@ rm -Rf feeds/packages/utils/cgroupfs-mount
 ./scripts/feeds install -a
 sed -i 's/Os/O2/g' include/target.mk
 rm -rf target/linux package/kernel include/{kernel-version.mk,kernel-defaults.mk}
-svn export https://github.com/openwrt/openwrt/trunk/target/linux target/linux
+svn export https://github.com/rsalvaterra/openwrt/branches/5.10-bump/target/linux target/linux
+wget -O include/kernel-version.mk https://raw.githubusercontent.com/rsalvaterra/openwrt/5.10-bump/include/kernel-version.mk
 svn export https://github.com/openwrt/openwrt/trunk/package/kernel package/kernel
-wget -O include/kernel-version.mk https://raw.githubusercontent.com/openwrt/openwrt/master/include/kernel-version.mk
 wget -O include/kernel-defaults.mk https://raw.githubusercontent.com/openwrt/openwrt/master/include/kernel-defaults.mk
 rm -Rf tools/upx && svn export https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
 rm -Rf tools/ucl && svn export https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
