e9a5746632d6a9e87e47e01749fd3f436aa48463
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2021-12-04 13:07:54 +0800

    update

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/x86/patches-5.4 target/linux/x86/patches-5.4
+rm -rf target/linux/x86/patches-5.4/.svn
+
 sed -i 's,kmod-r8169,kmod-r8168,g' target/linux/x86/image/64.mk
 
 echo '
