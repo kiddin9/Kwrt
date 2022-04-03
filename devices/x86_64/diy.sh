767c2c3d3bf4e6d99c2bc3cc041b462b6b13877a
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-04-03 11:56:02 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -10,8 +10,6 @@ sed -i 's,kmod-r8169,kmod-r8168,g' target/linux/x86/image/64.mk
 echo '
 CONFIG_CRYPTO_CHACHA20_X86_64=y
 CONFIG_CRYPTO_POLY1305_X86_64=y
-CONFIG_DRM=y
-CONFIG_DRM_I915=y
 ' >> ./target/linux/x86/config-5.10
 
 sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
