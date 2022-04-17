42e355c6eda4bcfbcc1ab2010f029f61498d09ad
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-04-17 10:01:22 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -25,9 +25,6 @@ coremark wget-ssl curl htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr bash \
 kmod-usb2 kmod-usb3 automount /" include/target.mk
 sed -i "/dnsmasq \\\/d" include/target.mk
 
-curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/kernel/linux/modules/video.mk -o package/kernel/linux/modules/video.mk
-sed -i "s/+PACKAGE_kmod-backlight.*//" package/kernel/linux/modules/video.mk
-
 sh -c "curl -sfL https://github.com/coolsnowwolf/lede/commit/af9ddeb7c95186854733262554c944d29513a58a.patch | patch -d './' -p1 --forward"
 sh -c "curl -sfL https://github.com/coolsnowwolf/lede/commit/b4a6d7f974f7b17052ade15a3cf63086bd52736d.patch | patch -d './' -p1 --forward"
 sh -c "curl -sfL https://github.com/coolsnowwolf/lede/commit/06fcdca1bb9c6de6ccd0450a042349892b372220.patch | patch -d './' -p1 --forward"
