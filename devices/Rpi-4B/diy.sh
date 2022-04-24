bd41bbe101c643f6b781357baf3562cf8b07ea0e
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-04-24 10:29:02 +0800

    update

--- devices/Rpi-4B/diy.sh
+++ devices/Rpi-4B/diy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sh -c "curl -sfL https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/9675.patch | patch -d './' -p1 --forward"
+kernel_v="$(cat include/kernel-5.10 | grep LINUX_KERNEL_HASH-* | cut -f 2 -d - | cut -f 1 -d ' ')"
+echo "KERNEL=${kernel_v}" >> $GITHUB_ENV || true
+sed -i "s?packages/%A/kmods/.*?packages/%A/kmods/$kernel_v?" include/feeds.mk
+
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic/hack-5.10 target/linux/generic/hack-5.10
+rm -rf target/linux/generic/hack-5.10/{220-gc_sections*,781-dsa-register*,780-drivers-net*}
 
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += my-autocore-arm luci-app-cpufreq kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152/' target/linux/bcm27xx/Makefile
 
@@ -27,4 +32,4 @@ CONFIG_REALTEK_PHY=y
 CONFIG_CPU_FREQ_GOV_USERSPACE=y
 CONFIG_CPU_FREQ_GOV_ONDEMAND=y
 CONFIG_CPU_FREQ_GOV_CONSERVATIVE=y
-' >> ./target/linux/bcm27xx/bcm2711/config-5.15
+' >> ./target/linux/bcm27xx/bcm2711/config-5.10
