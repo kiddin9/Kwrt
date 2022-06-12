25fd72c2af8521e46139c081817c44c0d2eee709
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-06-13 03:25:19 +0800

    update

--- devices/ramips_mt7621/diy.sh
+++ devices/ramips_mt7621/diy.sh
@@ -2,7 +2,7 @@
 
 shopt -s extglob
 
-curl -sfL https://raw.githubusercontent.com/x-wrt/x-wrt/22.03/target/linux/ramips/patches-5.10/995-0001-hwnat-add-natflow-flow-offload-support.patch -o target/linux/ramips/patches-5.10/995-0001-hwnat-add-natflow-flow-offload-support.patch
+curl -sfL https://raw.githubusercontent.com/x-wrt/x-wrt/master/target/linux/ramips/patches-5.10/995-0001-hwnat-add-natflow-flow-offload-support.patch -o target/linux/ramips/patches-5.10/995-0001-hwnat-add-natflow-flow-offload-support.patch
 
 svn export --force https://github.com/x-wrt/x-wrt/trunk/target/linux/ramips/files/drivers/net/ethernet/ralink target/linux/ramips/files/drivers/net/ethernet/ralink
 
