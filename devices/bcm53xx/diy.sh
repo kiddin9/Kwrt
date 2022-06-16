97cae95d85a7c465c45e3df4db1a8d2fe1e29989
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-06-16 19:20:26 +0800

    update

--- devices/bcm53xx/diy.sh
+++ devices/bcm53xx/diy.sh
@@ -2,6 +2,8 @@
 
 shopt -s extglob
 
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+bash $SHELL_FOLDER/../common/kernel_5.15.sh
 
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += my-autocore-arm luci-app-cpufreq luci-app-turboacc/' target/linux/mediatek/Makefile
 
