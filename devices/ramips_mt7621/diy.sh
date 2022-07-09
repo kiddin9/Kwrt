a28d4c5471d0206b949d128605cc35e7d7f15ae7
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-07-10 05:51:15 +0800

    update

--- devices/ramips_mt7621/diy.sh
+++ devices/ramips_mt7621/diy.sh
@@ -2,9 +2,5 @@
 
 shopt -s extglob
 
-#rm -Rf target/linux/{ramips,generic}
-#svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
-#svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic target/linux/generic
 
-#curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/master/include/kernel-5.4 -o include/kernel-5.4
 
