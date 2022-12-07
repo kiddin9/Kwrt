737dbaa227b0b5799a0f98561e48d5646b03e4ea
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-12-08 04:27:00 +0800

    update

--- devices/mediatek_mt7981/diy.sh
+++ devices/mediatek_mt7981/diy.sh
@@ -6,7 +6,7 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 kernel_v="$(cat include/kernel-version.mk | grep LINUX_KERNEL_HASH-5.* | cut -f 2 -d - | cut -f 1 -d ' ')"
 echo "KERNEL=${kernel_v}" >> $GITHUB_ENV || true
-sed -i "s?targets/%S/packages?targets/%S/$kernel_v?" include/feeds.mk
+sed -i "s?targets/%S/.*'?targets/%S/$kernel_v'?" include/feeds.mk
 
 rm -rf devices/common/patches/{imagebuilder.patch,iptables.patch,kernel-defaults.patch,targets.patch}
 
