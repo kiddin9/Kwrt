bef020db6a4e68367aadae8625cb72274e2c5728
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-01-28 00:24:27 +0800

    update

--- devices/ipq807x/diy.sh
+++ devices/ipq807x/diy.sh
@@ -3,9 +3,7 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-#bash $SHELL_FOLDER/../common/kernel_5.15.sh
-
-svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic/hack-5.15 target/linux/generic/hack-5.15
+bash $SHELL_FOLDER/../common/kernel_5.15.sh
 
 rm -rf feeds/kiddin9/{rtl8821cu,rtl88x2bu} package/kernel/mt76
 
