7016d51c1be442dc8c6827afb87bd535aaa4fd38
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-01-29 05:16:50 +0800

    update

--- devices/ipq807x/diy.sh
+++ devices/ipq807x/diy.sh
@@ -3,12 +3,13 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-bash $SHELL_FOLDER/../common/kernel_5.15.sh
+#bash $SHELL_FOLDER/../common/kernel_5.15.sh
+
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic/hack-5.15 target/linux/generic/hack-5.15
 
 rm -rf feeds/kiddin9/{rtl8821cu,rtl88x2bu} package/kernel/mt76
 
-git clone https://github.com/robimarko/nss-packages --depth 1 package/nss-packages
-
+curl -sfL https://raw.githubusercontent.com/Boos4721/openwrt/master/target/linux/ipq807x/patches-5.15/700-ipq8074-overclock-cpu-2.2ghz.patch -o target/linux/ipq807x/patches-5.15/700-ipq8074-overclock-cpu-2.2ghz.patch
 
 echo "
 CONFIG_PACKAGE_kmod-ipt-coova=n
