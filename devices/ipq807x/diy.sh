39d3e653d8ee5ddf5635de55f6d3b9db7e15f666
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-01-29 05:41:59 +0800

    update

--- devices/ipq807x/diy.sh
+++ devices/ipq807x/diy.sh
@@ -9,8 +9,6 @@ svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/generic/hack-5.15
 
 rm -rf feeds/kiddin9/{rtl8821cu,rtl88x2bu} package/kernel/mt76
 
-curl -sfL https://raw.githubusercontent.com/Boos4721/openwrt/master/target/linux/ipq807x/patches-5.15/700-ipq8074-overclock-cpu-2.2ghz.patch -o target/linux/ipq807x/patches-5.15/700-ipq8074-overclock-cpu-2.2ghz.patch
-
 echo "
 CONFIG_PACKAGE_kmod-ipt-coova=n
 CONFIG_PACKAGE_kmod-pf-ring=n
