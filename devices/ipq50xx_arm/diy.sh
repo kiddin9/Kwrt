04756cdeda80e6ca6f9e1228da451dc79fc7633f
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-06-01 06:54:06 +0800

    update

--- devices/ipq50xx_arm/diy.sh
+++ devices/ipq50xx_arm/diy.sh
@@ -4,7 +4,7 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/feeds/kiddin9/rtl* feeds/kiddin9/{shortcut-fe,fullconenat-nft} package/kernel/mt76 package/kernel/exfat package/feeds/packages/fuse*
+rm -rf package/feeds/kiddin9/rtl* feeds/kiddin9/{shortcut-fe,fullconenat-nft} package/kernel/mt76 package/kernel/exfat package/feeds/packages/fuse* package/feeds/luci/ucode-mod-html package/feeds/luci/rpcd-mod-luci
 
 rm -rf devices/common/patches/{fix.patch,iptables.patch,kernel-defaults.patch,targets.patch}
 
@@ -13,4 +13,5 @@ rm -rf toolchain/musl package/utils/e2fsprogs package/libs/libselinux package/fe
 svn co https://github.com/openwrt/openwrt/branches/openwrt-23.05/toolchain/musl toolchain/musl
 svn co https://github.com/openwrt/openwrt/branches/openwrt-23.05/package/utils/e2fsprogs package/utils/e2fsprogs
 svn co https://github.com/openwrt/openwrt/branches/openwrt-23.05/package/libs/libselinux package/libs/libselinux
-ln -sf $(pwd)/feeds/luci/modules/luci-base package/feeds/kiddin9/
+svn co https://github.com/openwrt/openwrt/branches/openwrt-23.05/package/utils/ucode package/utils/ucode
+#ln -sf $(pwd)/feeds/luci/modules/luci-base package/feeds/kiddin9/
