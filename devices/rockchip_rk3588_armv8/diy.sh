2e67806eb4046ab4ac60a6fbe66a1c99bd09923f
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-07-09 04:22:15 +0800

    update

--- devices/rockchip_rk3588_armv8/diy.sh
+++ devices/rockchip_rk3588_armv8/diy.sh
@@ -12,6 +12,8 @@ echo '
 CONFIG_SENSORS_PWM_FAN=y
 ' >> ./target/linux/rockchip/armv8/config-5.10
 
+sed -i "/KernelPackage,ptp/d" package/kernel/linux/modules/other.mk
+
 mv -f target/linux/rockchip target/linux/rockchip_rk3588
 
 rm -rf package/feeds/packages/glib2 package/devel/perf package/feeds/kiddin9/{shortcut-fe,fibocom_QMI_WWAN,oaf,fast-classifier,firewall,rtl88x2bu,rtl8821cu} package/feeds/packages/bluez package/kernel/ksmbd package/feeds/routing/batman-adv
