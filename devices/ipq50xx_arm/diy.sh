c000fcbc0f5e93049fe9e3545240e42627d1e954
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-08-12 00:39:00 +0800

    update

--- devices/ipq50xx_arm/diy.sh
+++ devices/ipq50xx_arm/diy.sh
@@ -3,5 +3,3 @@
 shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-
-sed -i "s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=2024-12-06/" package/network/config/netifd/Makefile
--- devices/ipq60xx_generic/diy.sh
+++ devices/ipq60xx_generic/diy.sh
@@ -3,9 +3,5 @@ shopt -s extglob
 
 svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ipq60xx target/linux/ipq60xx
 
-git clone https://github.com/robimarko/nss-packages package/nss-packages
-svn co https://github.com/coolsnowwolf/lede/trunk/package/qca package/nss-packages/qca
-mv -f package/nss-packages/qca/nss/* package/nss-packages/qca/
-rm -rf package/nss-packages/qca/qca-ssdk package/nss-packages/qca/qca-nss-dp package/nss-packages/qca/qca-*-64 package/nss-packages/qca/nss
+svn co https://github.com/coolsnowwolf/lede/trunk/package/qca package/qca
 
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-qca-nss-dp kmod-qca-nss-drv kmod-qca-nss-drv-pppoe nss-firmware-ipq6018 qca-nss-ecm/' target/linux/ipq60xx/Makefile
