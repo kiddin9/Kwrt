cd04525ccdb9991e6b817c72fe6ce606107aff80
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-09-13 18:53:51 +0800

    update

--- devices/meson_meson8b/diy.sh
+++ devices/meson_meson8b/diy.sh
@@ -4,7 +4,7 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf target/linux/generic feeds/routing/batman-adv
+rm -rf feeds/routing/batman-adv
 
 svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/meson target/linux/meson
 
