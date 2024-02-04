f7490926a97b3027423253240d6c57b450646e61
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-02-04 17:23:57 +0800

    update

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -16,7 +16,7 @@ sed -i '/	refresh_config();/d' scripts/feeds
 echo "$(date +"%s")" >version.date
 sed -i '/$(curdir)\/compile:/c\$(curdir)/compile: package/opkg/host/compile' package/Makefile
 sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite --force-depends/' package/Makefile
-sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-app-advanced luci-app-firewall luci-app-gpsysupgrade luci-app-opkg luci-app-upnp luci-app-autoreboot \
+sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-app-advancedplus luci-app-firewall luci-app-gpsysupgrade luci-app-opkg luci-app-upnp luci-app-autoreboot \
 luci-app-wizard luci-base luci-compat luci-lib-ipkg luci-lib-fs \
 coremark wget-ssl curl autocore htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr bash openssh-sftp-server block-mount resolveip ds-lite swconfig luci-app-fan /" include/target.mk
 sed -i "s/procd-ujail//" include/target.mk
--- devices/mediatek_filogic/diy.sh
+++ devices/mediatek_filogic/diy.sh
@@ -5,3 +5,5 @@ shopt -s extglob
 sed -i "/mt7986_xiaomi_redmi-router-ax6000 /d" package/boot/uboot-mediatek/Makefile
 sed -i "/mt7981_h3c_magic-nx30-pro /d" package/boot/uboot-mediatek/Makefile
 sed -i "/mt7981_qihoo_360t7 /d" package/boot/uboot-mediatek/Makefile
+sed -i "/cmcc_rax3000m.* /d" package/boot/uboot-mediatek/Makefile
+sed -i "/mt7981_jcg_q30-pro /d" package/boot/uboot-mediatek/Makefile
