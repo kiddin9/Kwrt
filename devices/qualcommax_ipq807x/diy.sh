7590ceb4f12b4da002166cabd47a1fa6e4072b72
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-03-25 06:39:50 +0800

    update

--- devices/qualcommax_ipq807x/diy.sh
+++ devices/qualcommax_ipq807x/diy.sh
@@ -4,3 +4,5 @@ shopt -s extglob
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 bash $SHELL_FOLDER/../common/kernel_6.1.sh
+
+git_clone_path master https://github.com/openwrt/openwrt target/linux/qualcommax
