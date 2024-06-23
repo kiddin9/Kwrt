d9941b1aad690dd4c8749deaf44bc104972bbf60
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-06-24 03:16:47 +0800

    update

--- devices/mediatek_filogic/diy.sh
+++ devices/mediatek_filogic/diy.sh
@@ -10,3 +10,9 @@ sed -i "s/mi-router-wr30u-stock/mi-router-wr30u/" target/linux/mediatek/filogic/
 sed -i "s/mi-router-wr30u-stock/mi-router-wr30u/" target/linux/mediatek/filogic/base-files/etc/board.d/02_network
 sed -i "s/mi-router-wr30u-stock/mi-router-wr30u/" target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
 
+sed -i "s/redmi-router-ax6000-stock/redmi-router-ax6000/" package/boot/uboot-envtools/files/mediatek_filogic
+sed -i "s/redmi-router-ax6000-stock/redmi-router-ax6000/" target/linux/mediatek/filogic/base-files/etc/board.d/01_leds
+sed -i "s/redmi-router-ax6000-stock/redmi-router-ax6000/" target/linux/mediatek/filogic/base-files/etc/board.d/02_network
+sed -i "s/redmi-router-ax6000-stock/redmi-router-ax6000/" target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+sed -i "s/redmi-router-ax6000-stock/redmi-router-ax6000/" target/linux/mediatek/base-files/lib/preinit/05_set_preinit_iface
+
