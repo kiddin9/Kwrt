b122467f2c2891766c9478df69d4ac6e94a2c140
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-08-21 04:27:03 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,10 +4,14 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-bash $SHELL_FOLDER/../common/kernel_6.1.sh
+bash $SHELL_FOLDER/../common/kernel_6.6.sh
 
-rm -rf target/linux/qualcommax package/kernel/qca-* devices/common/patches/qca-ssdk.patch package/boot/uboot-envtools package/firmware/ipq-wifi
-git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax package/firmware/ipq-wifi package/boot/uboot-envtools package/qca
+rm -rf package/boot/uboot-envtools package/firmware/ipq-wifi
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax/ipq60xx package/firmware/ipq-wifi package/boot/uboot-envtools
+
+git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/qualcommax/files/arch/arm64/boot/dts/qcom
+
+wget -N https://github.com/coolsnowwolf/lede/raw/master/target/linux/qualcommax/image/ipq60xx.mk -P target/linux/qualcommax/image/
 
 sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/qualcommax/Makefile
 
