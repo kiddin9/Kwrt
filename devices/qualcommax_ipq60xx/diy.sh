823d695f418a9905b97b5db64507c3659ff92b2a
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-08-22 02:13:51 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -9,7 +9,7 @@ bash $SHELL_FOLDER/../common/kernel_6.1.sh
 rm -rf target/linux/qualcommax target/linux/generic/!(*-5.15) package/kernel/qca-* devices/common/patches/qca-ssdk.patch package/boot/uboot-envtools package/firmware/ipq-wifi
 git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax package/firmware/ipq-wifi package/boot/uboot-envtools package/qca
 
-git_clone_path master ttps://github.com/coolsnowwolf/lede mv target/linux/generic
+git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/generic
 
 wget -N https://github.com/immortalwrt/immortalwrt/raw/master/include/kernel-6.1 -P include/
 
