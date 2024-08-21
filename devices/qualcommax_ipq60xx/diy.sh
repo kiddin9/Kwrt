dfaf0a695133d3618dde4b60234fb4ecfaeddf68
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-08-22 04:34:02 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -6,10 +6,12 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 bash $SHELL_FOLDER/../common/kernel_6.1.sh
 
-rm -rf target/linux/qualcommax target/linux/generic/!(*-5.15) package/kernel/qca-* devices/common/patches/qca-ssdk.patch package/boot/uboot-envtools package/firmware/ipq-wifi
+rm -rf target/linux/qualcommax target/linux/generic/!(*-5.15) package/kernel/qca-* package/kernel/mac80211 devices/common/patches/qca-ssdk.patch package/boot/uboot-envtools package/firmware/ipq-wifi
 git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax package/firmware/ipq-wifi package/boot/uboot-envtools package/qca
 
 git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/generic
+
+git_clone_path master https://github.com/coolsnowwolf/lede package/kernel/mac80211
 
 wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.1 -P include/
 
