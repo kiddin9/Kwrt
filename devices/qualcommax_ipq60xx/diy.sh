5996fff9038e17df9220c669aaa4f88ac6081ec4
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-08-21 22:42:14 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,16 +4,14 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-bash $SHELL_FOLDER/../common/kernel_6.6.sh
+bash $SHELL_FOLDER/../common/kernel_6.1.sh
 
-rm -rf package/boot/uboot-envtools package/firmware/ipq-wifi
-git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax/ipq60xx package/firmware/ipq-wifi package/boot/uboot-envtools
+rm -rf target/linux/qualcommax target/linux/generic/!(*-5.15) package/kernel/qca-* devices/common/patches/qca-ssdk.patch package/boot/uboot-envtools package/firmware/ipq-wifi
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax package/firmware/ipq-wifi package/boot/uboot-envtools package/qca
 
-git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/qualcommax/files/arch/arm64/boot/dts/qcom
+git_clone_path master ttps://github.com/coolsnowwolf/lede mv target/linux/generic
 
-git_clone_path ipq60xx-devel https://github.com/JiaY-shi/openwrt mv target/linux/qualcommax/patches-6.6
-
-wget -N https://github.com/coolsnowwolf/lede/raw/master/target/linux/qualcommax/image/ipq60xx.mk -P target/linux/qualcommax/image/
+wget -N https://github.com/immortalwrt/immortalwrt/raw/master/include/kernel-6.1 -P include/
 
 sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/qualcommax/Makefile
 
