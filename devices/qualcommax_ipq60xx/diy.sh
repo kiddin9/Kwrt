a92b21cbe346c7b9efcbd5fbb2e7781bbfc67bb5
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-08-22 11:25:33 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -11,7 +11,7 @@ git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcomm
 
 git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/generic
 
-git_clone_path master https://github.com/coolsnowwolf/lede package/kernel/mac80211
+git_clone_path c640f7b93736621b4d56627e4f6ab824093f9c3d https://github.com/openwrt/openwrt package/kernel/mac80211
 
 wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.1 -P include/
 
