ce1fe99b4f900f9eae3cef24e92e5e8fd2fb15c2
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-09-03 18:36:17 +0800

    update

--- devices/amlogic_meson8b/diy.sh
+++ devices/amlogic_meson8b/diy.sh
@@ -12,6 +12,8 @@ git_clone_path c640f7b93736621b4d56627e4f6ab824093f9c3d https://github.com/openw
 
 git_clone_path main https://github.com/shiyu1314/openwrt-onecloud kernel/6.1/amlogic && mv -f kernel/6.1/amlogic target/linux/
 
+sed -i "s/CPU_SUBTYPE:=neon-vfpv4/CPU_SUBTYPE:=vfpv4/" target/linux/amlogic/meson8b/target.mk
+
 sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/amlogic/image/meson8b.mk
 
 
