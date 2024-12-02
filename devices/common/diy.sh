bfc17ae3a1eb278f5b1d8e94a9b5ddc80eba4bfe
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-12-03 00:45:45 +0800

    update

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -13,8 +13,9 @@ sed -i '/	refresh_config();/d' scripts/feeds
 ./scripts/feeds install -a -p kiddin9 -f
 ./scripts/feeds install -a
 
-rm -rf package/base-files
+rm -rf package/base-files feeds/luci/modules/luci-base
 mv -f feeds/kiddin9/base-files package/
+mv -f feeds/kiddin9/luci-base feeds/luci/modules/
 
 echo "$(date +"%s")" >version.date
 sed -i '/$(curdir)\/compile:/c\$(curdir)/compile: package/opkg/host/compile' package/Makefile
