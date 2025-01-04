602401f840238d70159bb1279fd53b6a41d18998
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-01-05 06:22:15 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,9 +4,9 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/firmware package/boot/uboot-envtools package/kernel/mac80211 target/linux/qualcommax
+rm -rf package/firmware package/boot/uboot-envtools package/kernel/mac80211 package/kernel/qca-* package/kernel/nat46 target/linux/qualcommax
 
-git_clone_path openwrt-24.10 https://github.com/LiBwrt-op/openwrt-6.x package/firmware package/boot/uboot-envtools package/kernel/mac80211 target/linux/qualcommax
+git_clone_path openwrt-24.10 https://github.com/LiBwrt-op/openwrt-6.x package/firmware package/boot/uboot-envtools package/kernel/mac80211 package/kernel/qca-nss-dp package/kernel/qca-ssdk package/kernel/nat46 target/linux/qualcommax
 
 git clone https://github.com/qosmio/nss-packages package/feeds/nss-packages
 git clone https://github.com/qosmio/sqm-scripts-nss package/feeds/sqm-scripts-nss
