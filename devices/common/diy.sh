34e4d5aa5dd8ab01f3c0fbced377b5df6430eac6
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-01-28 15:54:08 +0800

    update

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -54,7 +54,7 @@ wget -N https://raw.githubusercontent.com/openwrt/packages/master/lang/golang/go
 #sed -i "/call Build\/check-size,\$\$(KERNEL_SIZE)/d" include/image.mk
 
 git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/generic/hack-6.6
-rm -rf target/linux/generic/hack-6.6/929-Revert-genetlink*
+rm -rf target/linux/generic/hack-6.6/929-Revert-genetlink* target/linux/generic/hack-6.6/767-net-phy-realtek-add-led*
 wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/pending-6.6/613-netfilter_optional_tcp_window_check.patch -P target/linux/generic/pending-6.6/
 
 wget -N https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/16414.patch -P devices/common/patches/
