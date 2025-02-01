e9a25dfdd28db18bbc111bbc9485134c6f07afb4
Louis <60611553+359139098@users.noreply.github.com>
2025-02-02 07:14:21 +0800

    Delete devices directory

--- devices/bcm27xx_bcm2711/diy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-
-#bash $SHELL_FOLDER/../common/kernel_6.1.sh
-
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += fdisk lsblk kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152/' target/linux/bcm27xx/Makefile
-
-
--- devices/qualcommax_ipq60xx/README.md
+++
@@ -1,1 +0,0 @@
-源码来源 https://github.com/LiBwrt-op/openwrt-6.x 感谢
