98a578cc8e0fc99739bb02d210d7ccf1f202bad8
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-05-30 20:34:04 +0800

    update

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -4,16 +4,8 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/kernel package/boot/uboot-envtools package/firmware/ipq-wifi package/firmware/ath11k-firmware target/linux/qualcommax target/linux/generic
+rm -rf package/boot package/firmware/ipq-wifi package/firmware/ath11k-firmware target/linux/qualcommax target/linux/generic
 
-git_clone_path main https://github.com/openwrt/openwrt package/kernel target/linux/qualcommax target/linux/generic package/boot/uboot-tools package/firmware/ipq-wifi package/firmware/ath11k-firmware
+git_clone_path main https://github.com/coolsnowwolf/lede target/linux/qualcommax target/linux/generic package/boot package/firmware/ipq-wifi package/firmware/ath11k-firmware
 
-wget -N https://github.com/openwrt/openwrt/raw/main/target/linux/generic/kernel-6.12 -P include/
-
-wget -N https://github.com/immortalwrt/immortalwrt/raw/refs/heads/master/package/network/utils/fullconenat-nft/patches/010-fix-build-with-kernel-6.12.patch -P package/feeds/kiddin9/fullconenat-nft/patches/
-
-git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/generic/hack-6.12
-
-wget -N https://github.com/coolsnowwolf/lede/raw/refs/heads/master/target/linux/generic/pending-6.12/613-netfilter_optional_tcp_window_check.patch -P target/linux/generic/pending-6.12/
-
-rm -rf package/feeds/kiddin9/{simulated-driver,fast-classifier,xtables-wgobfs} package/devel/perf package/feeds/packages/{ovpn-dco,xr_usb_serial_common,openvswitch,xtables-addons}
+wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.6 -P include/
