fdaa5437e9650241ec9d93f196032cc7839addc3
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-05-30 20:57:46 +0800

    update

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -6,6 +6,6 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 rm -rf package/boot package/firmware/ipq-wifi package/firmware/ath11k-firmware target/linux/qualcommax target/linux/generic
 
-git_clone_path main https://github.com/coolsnowwolf/lede target/linux/qualcommax target/linux/generic package/boot package/firmware/ipq-wifi package/firmware/ath11k-firmware
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax target/linux/generic package/boot package/firmware/ipq-wifi package/firmware/ath11k-firmware
 
 wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.6 -P include/
