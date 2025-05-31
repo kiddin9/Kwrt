777020291dbe4be825eb0459af48320db89c49a2
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-06-01 01:45:13 +0800

    update

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -13,9 +13,7 @@ wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.6 -P in
 sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/qualcommax/Makefile
 sed -i "/KERNEL_TESTING_PATCHVER/d" target/linux/qualcommax/Makefile
 
-make defconfig
-git_clone_path master https://github.com/coolsnowwolf/lede package/qca
 sed -i "/ECM_INTERFACE_MAP_T_ENABLE/d"  package/qca/qca-nss-ecm/Makefile
-sed -i "/CONFIG_ALL_NONSHARED=y/d" .config
 
-
+make defconfig
+sed -i "s/(CONFIG_PACKAGE_kmod-qca.*)=m/\1=n/g" .config
