d3d8daef0465f2f9d6f2b5a6f2906851bd3f249f
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-06-01 01:29:55 +0800

    update

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -7,12 +7,15 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 rm -rf package/boot package/kernel/qca* package/firmware/ipq-wifi package/firmware/ath11k-firmware target/linux/qualcommax target/linux/generic
 
 git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax target/linux/generic package/boot package/qca package/firmware/ipq-wifi package/firmware/ath11k-firmware
-rm -rf package/qca/qca-nss-crypto
 
 wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.6 -P include/
 
 sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/qualcommax/Makefile
 sed -i "/KERNEL_TESTING_PATCHVER/d" target/linux/qualcommax/Makefile
 
+make defconfig
+git_clone_path master https://github.com/coolsnowwolf/lede package/qca
+sed -i "/CONFIG_ALL_NONSHARED=y/d" .config
 sed -i "/ECM_INTERFACE_MAP_T_ENABLE/d"  package/qca/qca-nss-ecm/Makefile
-sed -i "/ECM_INTERFACE_OVPN_ENABLE/d"  package/qca/qca-nss-ecm/Makefile
+
+
