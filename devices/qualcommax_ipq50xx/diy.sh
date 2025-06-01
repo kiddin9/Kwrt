033598f429ab294208888dd0b1651e21e720fe30
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-06-01 13:54:46 +0800

    update

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -4,12 +4,16 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/boot/uboot-envtools package/firmware/ipq-wifi package/firmware/ath11k-firmware target/linux/qualcommax target/linux/generic package/kernel/mac80211 package/kernel/qca-ssdk package/kernel/qca-nss-dp
+rm -rf package/boot package/kernel/qca* package/firmware/ipq-wifi package/firmware/ath11k-firmware target/linux/qualcommax target/linux/generic
 
-git_clone_path main https://github.com/openwrt/openwrt target/linux/qualcommax target/linux/generic package/boot/uboot-tools package/firmware/ipq-wifi package/firmware/ath11k-firmware package/kernel/mac80211 package/kernel/qca-ssdk package/kernel/qca-nss-dp
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax target/linux/generic package/boot package/qca package/firmware/ipq-wifi package/firmware/ath11k-firmware
 
-wget -N https://github.com/openwrt/openwrt/raw/main/target/linux/generic/kernel-6.6 -P include/
+wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.6 -P include/
 
-git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/generic/hack-6.6
-rm -rf target/linux/generic/hack-6.6/{767-net-phy-realtek-add-led*,220-arm-gc_sections.patch}
-wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/pending-6.6/613-netfilter_optional_tcp_window_check.patch -P target/linux/generic/pending-6.6/
+sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/qualcommax/Makefile
+sed -i "/KERNEL_TESTING_PATCHVER/d" target/linux/qualcommax/Makefile
+
+sed -i "/ECM_INTERFACE_MAP_T_ENABLE/d"  package/qca/qca-nss-ecm/Makefile
+
+make defconfig
+sed -i "s/\(CONFIG_PACKAGE_kmod-qca.*\)=m/\1=n/g" .config
--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -7,18 +7,21 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 wget -N https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/openwrt-24.10/target/imagebuilder/files/Makefile -P target/imagebuilder/files/
 wget -N https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/openwrt-24.10/package/base-files/files/sbin/sysupgrade -P package/base-files/files/sbin/
 wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-24.10/package/network/config/firewall/Makefile -P package/network/config/firewall/
+wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-24.10/package/network/services/dnsmasq/files/dnsmasq.init -P package/network/services/dnsmasq/files/
+wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-24.10/package/network/config/wifi-scripts/files/lib/netifd/hostapd.sh -P package/network/config/wifi-scripts/files/lib/netifd/
+wget -N https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/main/include/target.mk -P include/
 
 git clone https://github.com/LiBwrt/nss-packages nss-packages
 
 mv -f nss-packages/* package/feeds/kiddin9/
 
-rm -rf feeds/kiddin9/shortcut-fe feeds/kiddin9/fibocom_QMI_WWAN feeds/kiddin9/quectel_QMI_WWAN feeds/kiddin9/xtables-wgobfs feeds/kiddin9/fullconenat-nft/
+sed -i "/ECM_INTERFACE_RAWIP_ENABLE/d"  package/feeds/kiddin9/qca-nss-ecm/Makefile
 
-rm -rf package/feeds/packages/ovpn-dco package/feeds/packages/xr_usb_serial_common package/feeds/packages/openvswitch package/feeds/packages/xtables-addons
+rm -rf feeds/kiddin9/{fullconenat-nft,xtables-wgobfs,shortcut-fe} package/devel/perf package/feeds/packages/{libpfring,ovpn-dco,xr_usb_serial_common,openvswitch,xtables-addons}
 
-rm -rf package/libs/libnftnl/patches/001-libnftnl-add-fullcone-expression-support.patch package/network/config/firewall4/patches/001-firewall4-Add-fullcone-support.patch
+rm -rf target/linux/generic/hack-6.6/220-arm-gc_sections.patch
 
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-qca-mcs kmod-qca-nss-drv-igs kmod-qca-nss-drv-l2tpv2 kmod-qca-nss-drv-lag-mgr kmod-qca-nss-drv-map-t kmod-qca-nss-drv-pppoe kmod-qca-nss-drv-pptp kmod-qca-nss-drv-qdisc kmod-qca-nss-macsec/' target/linux/qualcommax/Makefile
+sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/qualcommax/Makefile
 
 sed -i "s/LiBwrt/Kwrt/Ig" package/base-files/files/bin/config_generate package/base-files/image-config.in package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc config/Config-images.in Config.in include/u-boot.mk include/version.mk || true
 
