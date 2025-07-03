2842f0e383244e83ddda04dcae8002122ca3593c
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-07-04 06:42:02 +0800

    update

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -4,14 +4,16 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/boot package/kernel/qca* package/firmware/ipq-wifi package/firmware/ath11k-firmware target/linux/qualcommax target/linux/generic
+rm -rf package/boot package/kernel/qca* package/firmware/ipq-wifi package/firmware/ath11k-firmware target/linux/qualcommax target/linux/generic package/devel/perf
 
 git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax target/linux/generic package/boot package/qca package/firmware/ipq-wifi package/firmware/ath11k-firmware
 
 wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.6 -P include/
 
-sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/qualcommax/Makefile
-sed -i "/KERNEL_TESTING_PATCHVER/d" target/linux/qualcommax/Makefile
+sed -i -e "s/wpad-openssl/wpad-basic-mbedtls/" \
+	   -e "/KERNEL_TESTING_PATCHVER/d" \
+	   -e "s/KERNEL_PATCHVER:=6.12/KERNEL_PATCHVER:=6.6/" \
+target/linux/qualcommax/Makefile
 
 sed -i "/ECM_INTERFACE_MAP_T_ENABLE/d"  package/qca/qca-nss-ecm/Makefile
 
--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,25 +4,18 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-wget -N https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/openwrt-24.10/target/imagebuilder/files/Makefile -P target/imagebuilder/files/
-wget -N https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/openwrt-24.10/package/base-files/files/sbin/sysupgrade -P package/base-files/files/sbin/
-wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-24.10/package/network/config/firewall/Makefile -P package/network/config/firewall/
-wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-24.10/package/network/services/dnsmasq/files/dnsmasq.init -P package/network/services/dnsmasq/files/
-wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-24.10/package/network/config/wifi-scripts/files/lib/netifd/hostapd.sh -P package/network/config/wifi-scripts/files/lib/netifd/
-wget -N https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/main/include/target.mk -P include/
+rm -rf package/boot package/kernel/qca* package/firmware/ipq-wifi package/firmware/ath11k-firmware target/linux/qualcommax target/linux/generic package/devel/perf
 
-git clone https://github.com/LiBwrt/nss-packages nss-packages
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax target/linux/generic package/boot package/qca package/firmware/ipq-wifi package/firmware/ath11k-firmware
 
-mv -f nss-packages/* package/feeds/kiddin9/
+wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.6 -P include/
 
-sed -i "/ECM_INTERFACE_RAWIP_ENABLE/d"  package/feeds/kiddin9/qca-nss-ecm/Makefile
+sed -i -e "s/wpad-openssl/wpad-basic-mbedtls/" \
+	   -e "/KERNEL_TESTING_PATCHVER/d" \
+	   -e "s/KERNEL_PATCHVER:=6.12/KERNEL_PATCHVER:=6.6/" \
+target/linux/qualcommax/Makefile
 
-rm -rf feeds/kiddin9/{fullconenat-nft,xtables-wgobfs,shortcut-fe} package/devel/perf package/feeds/packages/{libpfring,ovpn-dco,xr_usb_serial_common,openvswitch,xtables-addons}
+sed -i "/ECM_INTERFACE_MAP_T_ENABLE/d"  package/qca/qca-nss-ecm/Makefile
 
-rm -rf target/linux/generic/hack-6.6/220-arm-gc_sections.patch
-
-sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/qualcommax/Makefile
-
-sed -i "s/LiBwrt/Kwrt/Ig" package/base-files/files/bin/config_generate package/base-files/image-config.in package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc config/Config-images.in Config.in include/u-boot.mk include/version.mk || true
-
-sed -i -e "s/set \${s}.country='\${country || ''}'/set \${s}.country='\${country || \"CN\"}'/g" -e "s/set \${s}.disabled=.*/set \${s}.disabled='0'/" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+make defconfig
+sed -i "s/\(CONFIG_PACKAGE_kmod-qca.*\)=m/\1=n/g" .config
