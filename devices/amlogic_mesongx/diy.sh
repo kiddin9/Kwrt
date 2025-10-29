51f684580ab78a49141cb161f7d2ace84804e22c
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-10-29 15:09:41 +0800

    update

--- devices/amlogic_mesongx/diy.sh
+++ devices/amlogic_mesongx/diy.sh
@@ -2,13 +2,21 @@
 
 shopt -s extglob
 
-SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+rm -rf package/kernel
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/generic/backport-6.1 target/linux/generic/hack-6.1 target/linux/generic/pending-6.1 target/linux/amlogic package/boot/uboot-amlogic package/kernel
 
-#bash $SHELL_FOLDER/../common/kernel_6.1.sh
+rm -rf package/kernel/bpf-headers
+git_clone_path openwrt-24.10 https://github.com/openwrt/openwrt package/kernel/bpf-headers
 
-git_clone_path master https://github.com/coolsnowwolf/lede target/linux/amlogic package/boot/uboot-amlogic
+sed -i "s/autocore-arm/autocore/" target/linux/amlogic/Makefile
 
-sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/amlogic/image/mesongx.mk
+wget -N https://github.com/coolsnowwolf/lede/raw/refs/heads/master/include/kernel-6.1 -P include/
+
+rm -rf feeds/routing/batman-adv package/kernel/rtw88-usb
+
+echo '
+CONFIG_BLK_DEV_INTEGRITY=n
+' >> target/linux/amlogic/mesongx/config-6.1
 
 
 
--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -30,11 +30,13 @@ wget -N https://github.com/immortalwrt/immortalwrt/raw/refs/heads/openwrt-24.10/
 wget -N https://github.com/immortalwrt/immortalwrt/raw/refs/heads/openwrt-24.10/package/network/utils/nftables/patches/001-drop-useless-file.patch -P package/network/utils/nftables/patches/
 wget -N https://github.com/immortalwrt/immortalwrt/raw/refs/heads/openwrt-24.10/package/libs/libnftnl/patches/001-libnftnl-add-fullcone-expression-support.patch -P package/libs/libnftnl/patches/
 wget -N https://github.com/immortalwrt/immortalwrt/raw/refs/heads/openwrt-24.10/package/firmware/wireless-regdb/patches/600-custom-change-txpower-and-dfs.patch -P package/firmware/wireless-regdb/patches/
+wget -N  https://github.com/coolsnowwolf/lede/raw/refs/heads/master/package/system/fstools/patches/0200-ntfs3-with-utf8.patch -P package/system/fstools/patches/
 wget -N https://github.com/immortalwrt/immortalwrt/raw/refs/heads/master/config/Config-kernel.in -P config/
 
 rm -rf package/libs/openssl package/network/services/ppp feeds/luci/modules/luci-mod-network
-git_clone_path openwrt-24.10 https://github.com/immortalwrt/immortalwrt package/libs/openssl package/network/services/ppp
-git_clone_path separate-dhcp-dns https://github.com/Alphix/luci modules/luci-mod-network && mv modules/luci-mod-network feeds/luci/modules/
+git_clone_path openwrt-24.10 https://github.com/immortalwrt/immortalwrt package/libs/openssl package/network/services/ppp 
+git_clone_path master https://github.com/openwrt/luci modules/luci-mod-network libs/rpcd-mod-luci; mv modules/luci-mod-network feeds/luci/modules/
+wget -N https://github.com/openwrt/luci/raw/refs/heads/master/libs/rpcd-mod-luci/src/luci.c -P feeds/luci/libs/rpcd-mod-luci/src/
 
 echo "$(date +"%s")" >version.date
 sed -i '/$(curdir)\/compile:/c\$(curdir)/compile: package/opkg/host/compile' package/Makefile
@@ -62,9 +64,6 @@ wget -N https://raw.githubusercontent.com/openwrt/packages/master/lang/golang/go
 #sed -i "/call Build\/check-size,\$\$(KERNEL_SIZE)/d" include/image.mk
 
 git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/generic/hack-6.6
-
-rm -rf package/system/fstools
-git_clone_path master https://github.com/coolsnowwolf/lede package/system/fstools
 
 rm -rf target/linux/generic/hack-6.6/767-net-phy-realtek-add-led*
 wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/pending-6.6/613-netfilter_optional_tcp_window_check.patch -P target/linux/generic/pending-6.6/
