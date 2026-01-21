7c183b92cd1aaf3b88312fe187d90202378afc6f
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2026-01-21 16:50:04 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,15 +4,13 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/boot package/firmware/ipq-wifi target/linux/qualcommax package/firmware/ath11k-firmware package/kernel/mac80211 package/kernel/nat46
+rm -rf package/boot package/firmware/ipq-wifi target/linux/generic target/linux/qualcommax package/firmware/ath11k-firmware package/kernel/mac80211 package/kernel/nat46
 
-git_clone_path 25.12-nss https://github.com/LiBwrt/openwrt-6.x target/linux/qualcommax package/boot package/firmware/ipq-wifi package/firmware/ath11k-firmware package/kernel/mac80211 package/kernel/nat46
+git_clone_path 25.12-nss https://github.com/LiBwrt/openwrt-6.x target/linux/generic target/linux/qualcommax package/boot package/firmware/ipq-wifi package/firmware/ath11k-firmware package/kernel/mac80211 package/kernel/nat46
 
 wget -N https://github.com/LiBwrt/openwrt-6.x/raw/refs/heads/25.12-nss/include/image-commands.mk -P include/
 wget -N https://github.com/LiBwrt/openwrt-6.x/raw/refs/heads/25.12-nss/config/Config-ipq.in -P config/
 wget -N https://github.com/LiBwrt/openwrt-6.x/raw/refs/heads/25.12-nss/Config.in -P ./
-
-rm -rf target/linux/generic/hack-6.12/{952-add-net-conntrack-events-support-multiple-registrant.patch,953-net-patch-linux-kernel-to-support-shortcut-fe.patch,980-mtd-silence-UBI-NAND-warnings.patch} target/linux/generic/pending-6.12/613-netfilter_optional_tcp_window_check.patch
 
 rm -rf feeds/kiddin9/shortcut-fe
 
