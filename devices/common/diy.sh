44affe0647799ca2ccfba7d3b280835d6c098d3d
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2026-09-20 05:40:36 +0800

    update

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -23,7 +23,7 @@ sed -i -e '$a /etc/bench.log' \
 sed -i -e '/^\/etc\/profile/d' \
         -e '/^\/etc\/shinit/d' \
         package/base-files/Makefile
-sed -i "s/192.168.1.1/192.168.1.2/" package/base-files/files/bin/config_generate
+sed -i "s/192.168.1/10.0.0/" package/base-files/files/bin/config_generate
 
 sed -i "s#false; \\\#true; \\\#" include/download.mk
 
